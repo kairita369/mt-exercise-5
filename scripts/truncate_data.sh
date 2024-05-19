5394ae80f9d72544230612e02bed1d0d95971d7e
kairita369 <kairita369@gmail.com>
2024-05-19 16:51:53 +0200

    fix in truncate script

--- scripts/truncate_data.sh
+++ scripts/truncate_data.sh
@@ -15,12 +15,14 @@ base=$scripts/..
 data_dir=$base/data
 output_dir=$base/data2
 
-if [ ! -f "$output_dir/$file1" ] || [ ! -f "$output_dir/$file2" ]; then
+echo $data_dir
+
+if [ ! -f "$data_dir/$file1" ] || [ ! -f "$data_dir/$file2" ]; then
     echo "One or both input files do not exist."
     exit 1
 fi
 
-head -n "$lines" "data_dir/$file1" | tee "$output_dir/${file1}"
-head -n "$lines" "data_dir/$file2" | tee "$output_dir/${file2}"
+head -n "$lines" "$data_dir/$file1" | tee "$output_dir/${file1}"
+head -n "$lines" "$data_dir/$file2" | tee "$output_dir/${file2}"
 
 echo "Extraction complete."
